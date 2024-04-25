c88504399a792274c7ebd54fa8a6c3097bf09728
Meshmanuu <meshanyangu@gmail.com>
2024-04-25 10:15:08 +0300

    lab done

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,12 @@
-const username = "Liza";
-const city = "New York";
+// import React from "react";
+// const username = "Liza";
+// const city = "New York";
 
-const image = "https://i.imgur.com/mV8PQxj.gif";
+// const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+// userData.js
+
+export const username = "Liza";
+export const city = "New York";
+export const image = "https://i.imgur.com/mV8PQxj.gif";
